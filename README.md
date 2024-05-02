0a7445073a9296646dba1e2f8399a7d044410f4e
Aetherinox <aetherinox@proton.me>
2024-05-02 07:00:09 -0700

    Initial upload

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
-# cloudflare-publish-action
+# Cloudflare Pages GitHub Action
+
+GitHub Action for creating Cloudflare Pages deployments, using the new [Direct Upload](https://developers.cloudflare.com/pages/platform/direct-upload/) feature and [Wrangler](https://developers.cloudflare.com/pages/platform/direct-upload/#wrangler-cli) integration.
+
+## Usage
+
+1. Create an API token in the Cloudflare dashboard with the "Cloudflare Pages — Edit" permission.
+1. Add that API token as a secret to your GitHub repository, `CLOUDFLARE_API_TOKEN`.
+1. Create a `.github/workflows/publish.yml` file in your repository:
+
+   ```yml
+   on: [push]
+
+   jobs:
+     publish:
+       runs-on: ubuntu-latest
+       permissions:
+         contents: read
+         deployments: write
+       name: Publish to Cloudflare Pages
+       steps:
+         - name: Checkout
+           uses: actions/checkout@v3
+
+         # Run a build step here if your project requires
+
+         - name: Publish to Cloudflare Pages
+           uses: cloudflare/pages-action@v1
+           with:
+             apiToken: ${{ secrets.CLOUDFLARE_API_TOKEN }}
+             accountId: YOUR_ACCOUNT_ID
+             projectName: YOUR_PROJECT_NAME
+             directory: YOUR_BUILD_OUTPUT_DIRECTORY
+             # Optional: Enable this if you want to have GitHub Deployments triggered
+             gitHubToken: ${{ secrets.GITHUB_TOKEN }}
+             # Optional: Switch what branch you are publishing to.
+             # By default this will be the branch which triggered this workflow
+             branch: main
+             # Optional: Change the working directory
+             workingDirectory: my-site
+             # Optional: Change the Wrangler version, allows you to point to a specific version or a tag such as `beta`
+             wranglerVersion: '3'
+   ```
+
+1. Replace `YOUR_ACCOUNT_ID`, `YOUR_PROJECT_NAME` and `YOUR_BUILD_OUTPUT_DIRECTORY` with the appropriate values to your Pages project.
+
+### Get account ID
+
+To find your account ID, log in to the Cloudflare dashboard > select your zone in Account Home > find your account ID in Overview under **API** on the right-side menu. If you have not added a zone, add one by selecting **Add site** . You can purchase a domain from [Cloudflare’s registrar](https://developers.cloudflare.com/registrar/).
+
+If you do not have a zone registered to your account, you can also get your account ID from the `pages.dev` URL. E.g: `https://dash.cloudflare.com/<ACCOUNT_ID>/pages`
+
+### Generate an API Token
+
+To generate an API token:
+
+1. Log in to the Cloudflare dashboard.
+2. Select My Profile from the dropdown menu of your user icon on the top right of your dashboard.
+3. Select API Tokens > Create Token.
+4. Under Custom Token, select Get started.
+5. Name your API Token in the Token name field.
+6. Under Permissions, select Account, Cloudflare Pages and Edit:
+7. Select Continue to summary > Create Token.
+
+More information can be found on [our guide for making Direct Upload deployments with continous integration](https://developers.cloudflare.com/pages/how-to/use-direct-upload-with-continuous-integration/#use-github-actions).
+
+### Specifying a branch
+
+The branch name is used by Cloudflare Pages to determine if the deployment is production or preview. Read more about
+[git branch build controls](https://developers.cloudflare.com/pages/platform/branch-build-controls/#branch-build-controls).
+
+If you are in a Git workspace, Wrangler will automatically pull the branch information for you. You can override this
+manually by adding the argument `branch: YOUR_BRANCH_NAME`.
+
+### Specifying a working directory
+
+By default Wrangler will run in the root package directory. If your app lives in a monorepo and you want to run Wrangler from its directory, add `workingDirectory: YOUR_PACKAGE_DIRECTORY`.
+
+### Wrangler v3
+
+You can use the newly released [Wrangler v3](https://blog.cloudflare.com/wrangler3/) with the `wranglerVersion` property.
+
+```yaml
+  - name: Publish to Cloudflare Pages
+    uses: cloudflare/pages-action@v1
+    with:
+      apiToken: ${{ secrets.CLOUDFLARE_API_TOKEN }}
+      accountId: YOUR_ACCOUNT_ID
+      projectName: YOUR_PROJECT_NAME
+      directory: YOUR_BUILD_OUTPUT_DIRECTORY
+      # Enable Wrangler v3
+      wranglerVersion: '3'
+```
+
+## Outputs
+
+| Name          | Description                                         |
+| ------------- | --------------------------------------------------- |
+| `id`          | The ID of the pages deployment                      |
+| `url`         | The URL of the pages deployment                     |
+| `alias`       | The alias if it exists otherwise the deployment URL |
+| `environment` | The environment that was deployed to                |
---
+++ src/index.ts
@@ -0,0 +1,179 @@
+import { getInput, setOutput, setFailed, summary } from "@actions/core";
+import type { Project, Deployment } from "@cloudflare/types";
+import { context, getOctokit } from "@actions/github";
+import shellac from "shellac";
+import { fetch } from "undici";
+import { env } from "process";
+import path from "node:path";
+
+type Octokit = ReturnType<typeof getOctokit>;
+
+try {
+	const apiToken = getInput("apiToken", { required: true });
+	const accountId = getInput("accountId", { required: true });
+	const projectName = getInput("projectName", { required: true });
+	const directory = getInput("directory", { required: true });
+	const gitHubToken = getInput("gitHubToken", { required: false });
+	const branch = getInput("branch", { required: false });
+	const workingDirectory = getInput("workingDirectory", { required: false });
+	const wranglerVersion = getInput("wranglerVersion", { required: false });
+
+	const getProject = async () => {
+		const response = await fetch(
+			`https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}`,
+			{ headers: { Authorization: `Bearer ${apiToken}` } }
+		);
+		if (response.status !== 200) {
+			console.error(`Cloudflare API returned non-200: ${response.status}`);
+			const json = await response.text();
+			console.error(`API returned: ${json}`);
+			throw new Error("Failed to get Pages project, API returned non-200");
+		}
+
+		const { result } = (await response.json()) as { result: Project | null };
+		if (result === null) {
+			throw new Error("Failed to get Pages project, project does not exist. Check the project name or create it!");
+		}
+
+		return result;
+	};
+
+	const createPagesDeployment = async () => {
+		// TODO: Replace this with an API call to wrangler so we can get back a full deployment response object
+		await shellac.in(path.join(process.cwd(), workingDirectory))`
+    $ export CLOUDFLARE_API_TOKEN="${apiToken}"
+    if ${accountId} {
+      $ export CLOUDFLARE_ACCOUNT_ID="${accountId}"
+    }
+  
+    $$ npx wrangler@${wranglerVersion} pages publish "${directory}" --project-name="${projectName}" --branch="${branch}"
+    `;
+
+		const response = await fetch(
+			`https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}/deployments`,
+			{ headers: { Authorization: `Bearer ${apiToken}` } }
+		);
+		const {
+			result: [deployment],
+		} = (await response.json()) as { result: Deployment[] };
+
+		return deployment;
+	};
+
+	const githubBranch = env.GITHUB_HEAD_REF || env.GITHUB_REF_NAME;
+
+	const createGitHubDeployment = async (octokit: Octokit, productionEnvironment: boolean, environment: string) => {
+		const deployment = await octokit.rest.repos.createDeployment({
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			ref: githubBranch || context.ref,
+			auto_merge: false,
+			description: "Cloudflare Pages",
+			required_contexts: [],
+			environment,
+			production_environment: productionEnvironment,
+		});
+
+		if (deployment.status === 201) {
+			return deployment.data;
+		}
+	};
+
+	const createGitHubDeploymentStatus = async ({
+		id,
+		url,
+		deploymentId,
+		environmentName,
+		productionEnvironment,
+		octokit,
+	}: {
+		octokit: Octokit;
+		id: number;
+		url: string;
+		deploymentId: string;
+		environmentName: string;
+		productionEnvironment: boolean;
+	}) => {
+		await octokit.rest.repos.createDeploymentStatus({
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			deployment_id: id,
+			// @ts-ignore
+			environment: environmentName,
+			environment_url: url,
+			production_environment: productionEnvironment,
+			log_url: `https://dash.cloudflare.com/${accountId}/pages/view/${projectName}/${deploymentId}`,
+			description: "Cloudflare Pages",
+			state: "success",
+			auto_inactive: false,
+		});
+	};
+
+	const createJobSummary = async ({ deployment, aliasUrl }: { deployment: Deployment; aliasUrl: string }) => {
+		const deployStage = deployment.stages.find((stage) => stage.name === "deploy");
+
+		let status = "⚡️  Deployment in progress...";
+		if (deployStage?.status === "success") {
+			status = "✅  Deploy successful!";
+		} else if (deployStage?.status === "failure") {
+			status = "🚫  Deployment failed";
+		}
+
+		await summary
+			.addRaw(
+				`
+# Deploying with Cloudflare Pages
+
+| Name                    | Result |
+| ----------------------- | - |
+| **Last commit:**        | \`${deployment.deployment_trigger.metadata.commit_hash.substring(0, 8)}\` |
+| **Status**:             | ${status} |
+| **Preview URL**:        | ${deployment.url} |
+| **Branch Preview URL**: | ${aliasUrl} |
+      `
+			)
+			.write();
+	};
+
+	(async () => {
+		const project = await getProject();
+
+		const productionEnvironment = githubBranch === project.production_branch || branch === project.production_branch;
+		const environmentName = `${projectName} (${productionEnvironment ? "Production" : "Preview"})`;
+
+		let gitHubDeployment: Awaited<ReturnType<typeof createGitHubDeployment>>;
+
+		if (gitHubToken && gitHubToken.length) {
+			const octokit = getOctokit(gitHubToken);
+			gitHubDeployment = await createGitHubDeployment(octokit, productionEnvironment, environmentName);
+		}
+
+		const pagesDeployment = await createPagesDeployment();
+		setOutput("id", pagesDeployment.id);
+		setOutput("url", pagesDeployment.url);
+		setOutput("environment", pagesDeployment.environment);
+
+		let alias = pagesDeployment.url;
+		if (!productionEnvironment && pagesDeployment.aliases && pagesDeployment.aliases.length > 0) {
+			alias = pagesDeployment.aliases[0];
+		}
+		setOutput("alias", alias);
+
+		await createJobSummary({ deployment: pagesDeployment, aliasUrl: alias });
+
+		if (gitHubDeployment) {
+			const octokit = getOctokit(gitHubToken);
+
+			await createGitHubDeploymentStatus({
+				id: gitHubDeployment.id,
+				url: pagesDeployment.url,
+				deploymentId: pagesDeployment.id,
+				environmentName,
+				productionEnvironment,
+				octokit,
+			});
+		}
+	})();
+} catch (thrown) {
+	setFailed(thrown.message);
+}
