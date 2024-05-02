3ae79ed6dd4cd64bc4aff64d042cbc53a339b24e
Aetherinox <aetherinox@proton.me>
2024-05-02 14:11:15 -0700

    ci: return

--- index.js
+++ index.js
@@ -25716,8 +25716,8 @@ try {
     }
 
     if ${npmLatest} {
-        npm install --global npm@latest
-        npm install --global wrangler
+        $ npm install --global npm@latest
+        $ npm install --global wrangler
     }
 
     $$ npx wrangler@${wranglerVersion} pages publish "${directory}" --project-name="${projectName}" --branch="${branch}" --skip-caching="${skipCaching}" --commit-message="${commitMsg}" --commit-dirty="${commitDirty}"
@@ -25732,8 +25732,8 @@ try {
     }
 
     if ${npmLatest} {
-        npm install --global npm@latest
-        npm install --global wrangler
+        $ npm install --global npm@latest
+        $ npm install --global wrangler
     }
 
     $$ npx wrangler@${wranglerVersion} pages deploy "${directory}" --project-name="${projectName}" --branch="${branch}" --skip-caching="${skipCaching}" --commit-message="${commitMsg}" --commit-dirty="${commitDirty}"
